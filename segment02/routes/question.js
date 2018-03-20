27850783b72e3e7a5cb2771c9b269ca2c9ce0ba7
huangke <huangke0802@163.com>
2018-03-20 23:56:02 +0800

    提问

--- segment02/routes/question.js
+++ segment02/routes/question.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var checkSession = require('../jsbean/CheckSession');
+var questionModel = require('../models/QuestionModel');
 
 router.all('/ask', function(req, res){
   loginBean = checkSession.check(req, res);
 
   if(!loginBean){
     return;
+  }
+
+  subflag = req.body['subflag'];
+  if (subflag == undefined) {
+    res.render('ask', { title: '提问题', loginBean: loginBean });
+  } else {
+    //发提问
+    questionModel.ask(req, res);
   }
 
 /*   if (loginBean == undefined){
